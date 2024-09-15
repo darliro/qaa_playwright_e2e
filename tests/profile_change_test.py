753da51ea94b688ba2642fdb8f22e39f99e630f6
Кускова Дарья Романовна <kuskova@tochka.com>
2024-09-15 11:25:49 +1000

    update profile change test for playwright

--- tests/profile_change_test.py
+++ tests/profile_change_test.py
@@ -8,25 +8,25 @@ class TestProfileFeature(BaseTest):
     @allure.title("Change Profile Name")
     @allure.severity("Critical")
     @pytest.mark.smoke
-    def test_change_profile_name(self, random_first_name):
-        """
-        Verifies that the user's profile name can be changed and saved.
-        """
+    def test_change_profile_name(self, page, random_first_name):
+        with allure.step("Open login page"):
+            self.login_page.open_page()
+
         with allure.step("Login and navigate to dashboard"):
-            self.login_page.open()
             self.login_page.enter_login(self.data.LOGIN)
             self.login_page.enter_password(self.data.PASSWORD)
             self.login_page.click_submit_button()
-            self.dashboard_page.verify_url()
+            self.dashboard_page.open_page()
 
         with allure.step("Navigate to personal info page"):
             self.dashboard_page.click_my_info_link()
-            self.personal_page.verify_url()
+            self.personal_page.open_page()
 
-        with allure.step(f"Change profile name to '{random_first_name}'"):
-            self.personal_page.change_name(random_first_name)
-            self.personal_page.save_changes()
+        with allure.step(f"Update profile name to '{random_first_name}'"):
+            self.personal_page.update_first_name(random_first_name)
+            self.personal_page.click_save_button()
 
-        with allure.step("Verify changes and take a screenshot"):
-            self.personal_page.verify_changes()
-            self.personal_page.make_screenshot("Profile name change success")
+        with allure.step("Verify profile name update"):
+            self.personal_page.verify_first_name_is_updated(random_first_name)
+            screenshot = page.screenshot()
+            allure.attach(screenshot, name="Profile_name_change_success", attachment_type=allure.attachment_type.PNG)
