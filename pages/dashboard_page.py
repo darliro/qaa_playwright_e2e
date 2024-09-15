fcc59905643b2b981881d8d84525adb1e0997794
Кускова Дарья Романовна <kuskova@tochka.com>
2024-09-15 11:24:51 +1000

    optimize dashboard page object structure

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,17 +1,19 @@
 import allure
+from playwright.sync_api import expect
 from base.base_page import BasePage
 from config.link_config import Links
-from selenium.webdriver.support import expected_conditions as EC
+from config.selectors_config import DASHBOARD_PAGE
 
 
 class DashboardPage(BasePage):
-    PAGE_URL = Links.DASHBOARD_PAGE
+    PAGE_URL: str = Links.DASHBOARD_PAGE
 
-    MY_INFO_BUTTON = ("xpath", "//span[text()='My Info']")
+    MY_INFO_BUTTON: str = DASHBOARD_PAGE["my_info_button"]
 
     @allure.step("Click on 'My Info' link")
-    def click_my_info_link(self):
-        """
-        Clicks on the 'My Info' link on the Dashboard page.
-        """
-        self.wait.until(EC.element_to_be_clickable(self.MY_INFO_BUTTON)).click()
+    def click_my_info_link(self) -> None:
+        self.page.locator(self.MY_INFO_BUTTON).click()
+
+    @allure.step("Verify that the 'My Info' URL is loaded")
+    def verify_my_info_url(self, expected_url: str) -> None:
+        expect(self.page).to_have_url(expected_url)
