e0043eff3e700baac309e9966947be35b39b33f3
darliro <glicerinn@gmail.com>
2024-09-17 12:39:52 +1000

    update selectors with better css selectors

--- config/selectors_config.py
+++ config/selectors_config.py
@@ -1,15 +1,15 @@
 LOGIN_PAGE = {
-    "username_field": "//input[@name='username']",
-    "password_field": "//input[@name='password']",
-    "submit_button": "//button[@type='submit']",
+    "username_input": "input[name='username']",
+    "password_input": "input[name='password']",
+    "submit_button": "button[type='submit']",
 }
 
 DASHBOARD_PAGE = {
-    "my_info_button": "//span[text()='My Info']",
+    "my_info_button": "span:has-text('My Info')",
 }
 
 PERSONAL_PAGE = {
-    "first_name_field": "//input[@name='firstName']",
+    "first_name_input": "input[name='firstName']",
     "save_button": "(//button[@type='submit'])[1]",
-    "spinner": "//div[@class='oxd-loading-spinner']",
+    "loading_spinner": ".oxd-loading-spinner",
 }
