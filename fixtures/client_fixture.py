e383a819e24934fc3edbb0a21ea696992bb2b4d6
darliro <glicerinn@gmail.com>
2024-09-19 12:06:36 +1000

    resolve Faker middle name generation issue

--- fixtures/client_fixture.py
+++ fixtures/client_fixture.py
@@ -8,3 +8,19 @@ def random_first_name() -> str:
     Fixture to generate a random first name.
     """
     return Faker().first_name()
+
+
+@pytest.fixture(scope="function")
+def random_last_name() -> str:
+    """
+    Fixture to generate a random last name.
+    """
+    return Faker().last_name()
+
+
+@pytest.fixture(scope="function")
+def random_middle_name() -> str:
+    """
+    Fixture to generate a random middle name.
+    """
+    return Faker().name_nonbinary()
