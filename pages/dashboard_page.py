dc73845a7de082c524aad32df1abf2598780ecbd
darliro <glicerinn@gmail.com>
2024-09-30 14:56:56 +1000

    optimize dashboard_page selectors and methods

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,17 +1,14 @@
 import allure
 from base.base_page import BasePage
 from config.url_config import Links
-from config.selectors_config import DASHBOARD_PAGE
 
 
 class DashboardPage(BasePage):
     PAGE_URL: str = Links.DASHBOARD_PAGE
 
-    MY_INFO_BUTTON: str = DASHBOARD_PAGE["my_info_button"]
-
     @allure.step("Click on 'My Info' link")
     def click_my_info_link(self) -> None:
-        self.page.locator(self.MY_INFO_BUTTON).click()
+        self.page.get_by_text("My info").click()
 
     @allure.step("Verify 'My Info' page is loaded")
     def verify_my_info_page_loaded(self) -> None:
